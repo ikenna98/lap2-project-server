91eafb0d420d60f591ab2c7f464b9827dd2aa1bd
Ikenna <ikennaagada@gmail.com>
2022-04-28 19:14:14 +0100

    fixed config

--- dbConfig.js
+++ dbConfig.js
@@ -1,3 +1,15 @@
-const { Pool } = require("pg");
-const pool = new Pool();
+const { Pool } = require('pg');
+
+let pool;
+if (process.env.NODE_ENV === 'production') {
+	pool = new Pool({
+		connectionString: process.env.DATABASE_URL,
+		ssl: {
+			rejectUnauthorized: false,
+		},
+	});
+} else {
+	pool = new Pool();
+}
+
 module.exports = pool;
